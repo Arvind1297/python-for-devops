3da1d033d59d1397c99d34851acd91a90a1ddb38
Arvind1297 <Arvind1297@gmail.com>
2026-05-08 14:14:31 +0530

    loop practice

--- question.2.py
+++ question.2.py
@@ -1,6 +1,5 @@
 #wap to take a number from user input and print formatted table
-num1 = int(input("enter your number:"))
+num1 = int(input("enter your number to print table:"))
 
 for i in range (1, 11):
-     num2=num1*i
-     print(f"{num1} x {i} = {num2}")
+     print(f"{num1}*{i} = {num1*i}")
--- question.3.py
+++ question.3.py
@@ -1,7 +1,6 @@
 #wap to take a number from user input
 #and print formatted table
-num1 = int(input("enter your number :"))
+num1 = int(input("enter your number to print table reverse :"))
 
 for i in range (10 , 0,-1):
-    num2 = num1*i
-    print(f"{num1}x{i}: {num2}")
+    print(f"{num1}*{i}: {num1*i}")
